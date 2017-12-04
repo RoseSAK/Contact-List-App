adc5fc1af7a88ae18f49b4c9686ddbdb63f2f886
RoseSAK <aquamooose@gmail.com>
2017-12-04 03:28:09 +0000

    improved search

--- contacts/models.py
+++ contacts/models.py
@@ -5,12 +5,10 @@ class Person(models.Model):
     address = models.CharField(max_length = 200, blank=True, null=True)
     contact_details = models.IntegerField(default=0, blank=True, null=True)
     department = models.CharField(max_length = 100, blank=True, null=True)
-    manager = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    manager = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name = 'manager_person')
 
     def __str__(self):
         return self.name
 
     def get_fields(self):
         return [(field.name, field.value_to_string(self)) for field in Person._meta.fields]
-
-# Create your models here.
--- contacts/views.py
+++ contacts/views.py
@@ -5,9 +5,10 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.template import loader, RequestContext
 from django.views import generic
-from django.db.models import Q
 
 from .models import Person
+from django.db.models import Q
+import operator, functools
 
 class IndexView(generic.ListView):
     context_object_name = 'person_list'
@@ -19,7 +20,20 @@ class DetailView(generic.DetailView):
     model = Person
 
 def search_results(request):
-    if request.method == 'GET': # If the form is submitted
-        search_query = request.GET.get('search_box', None)
-        results = Person.objects.filter(Q(name__icontains=search_query))
+    if request.method == 'GET': # If the search box submit button is used
+        search_query = request.GET.get('search_box', None) # get query term
+        results = Person.objects.filter(Q(name__icontains=search_query)
+                                        |Q(address__icontains=search_query)
+                                        |Q(department__icontains=search_query))
         return render(request,'contacts/search_results.html', {"results": results})
+
+# Attempts that didn't work
+        #search_fields = Person._meta.get_fields() # get fields to search each field
+        #search_fields = ['name__icontains', 'address__icontains', 'contact_details__contains', 'department__icontains']
+        #q_list = [(x, search_query) for x in search_fields]
+        #query_list = [Q(x) for x in q_list]
+        #q = [Q(x:query) for x in search_fields]
+        #for field_name in search_fields: #construct query object
+        #    q = Q(**{"%s" % field_name: search_query})
+        #    print(q)
+        #print(results)
